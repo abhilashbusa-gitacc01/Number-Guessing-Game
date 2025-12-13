abc9c3282ab4ed2bf265b1b7fcc57c0f819c6998
Abhilash Busa <abhilashbusa442@gmail.com>
2025-12-13 23:41:52 +0530

    Number Guessing Game

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -4,13 +4,22 @@ public class NumberGuessingGame {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
 
+         System.out.println("-----Number Guessing Game!------");
+        System.out.println();
+        System.out.println();
+        System.out.println("Choose levels between");
+        System.out.println("1. Low");
+        System.out.println("2. Medium");
+        System.out.println("3. High");
+        System.out.println("Choose Level: ");
+        int op = sc.nextInt();
+        switch(op){
+
+        case 1:
         int secret = rand.nextInt(50);
         int guess = 0;
         int attempts = 0;
 
-        System.out.println("-----Number Guessing Game!------");
-        System.out.println();
-        System.out.println();
         System.out.println("I am Guessing a number between 1 to 50");
         System.out.println("Try to Guess the number in 3 or less than 3 attempts");
 
@@ -40,6 +49,75 @@ public class NumberGuessingGame {
                     System.out.println("Correct!guessed in "+attempts+" attempts, but Your attempts are too High");
                 }
             }
+        }
+        case 2:
+        int sec = rand.nextInt(100);
+        int gue = 0;
+        int attem = 0;
+        System.out.println("I am Guessing a number between 1 to 100");
+        System.out.println("Try to Guess the number in 3 or less than 3 attempts");
+
+        while(gue != sec){
+            System.out.print("Enter your Guess: ");
+            gue = sc.nextInt();
+            attem++;
+            
+            if(gue < sec){
+                System.out.println("Too Low bro!");
+                System.out.println("Try again");
+                System.out.println();
+                
+            }else if(gue > sec){
+                System.out.println("Too High bro!");
+                System.out.println("Try again");
+                System.out.println();
+            }else{
+                if(attem == 1){
+                System.out.println("Correct! you guessed in first attempt");
+                }
+                else if(attem == 2){
+                    System.out.println("Correct! you guessed in second attempt");
+                }else if(attem == 3){
+                    System.out.println("Correct! you guessed in Third attempt");
+                }else if(attem >3){
+                    System.out.println("Correct!guessed in "+attem+" attempts, but Your attempts are too High");
+                }
+            }
+        }
+        case 3:
+        int s = rand.nextInt(500);
+        int gu = 0;
+        int atte = 0;
+        System.out.println("I am Guessing a number between 1 to 500");
+        System.out.println("Try to Guess the number in 3 or less than 3 attempts");
+
+        while(gu != s){
+            System.out.print("Enter your Guess: ");
+            gu = sc.nextInt();
+            atte++;
+            
+            if(gu < s){
+                System.out.println("Too Low bro!");
+                System.out.println("Try again");
+                System.out.println();
+                
+            }else if(gu > s){
+                System.out.println("Too High bro!");
+                System.out.println("Try again");
+                System.out.println();
+            }else{
+                if(atte == 1){
+                System.out.println("Correct! you guessed in first attempt");
+                }
+                else if(atte == 2){
+                    System.out.println("Correct! you guessed in second attempt");
+                }else if(atte == 3){
+                    System.out.println("Correct! you guessed in Third attempt");
+                }else if(atte >3){
+                    System.out.println("Correct!guessed in "+atte+" attempts, but Your attempts are too High");
+                }
+            }
+        }
         }
         sc.close();
 
